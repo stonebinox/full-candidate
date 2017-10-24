fb0a360b8cd70b9fc314453e701aecddaa001afd
stonebinox <anoop.santhanam@gmail.com>
2017-10-24 07:01:54 +0530

    Applications display

--- classes/applicationMaster.php
+++ classes/applicationMaster.php
@@ -89,6 +89,7 @@ class applicationMaster extends userMaster
             $am=$app['db']->fetchAssoc($am);
             for($i=0;$i<count($am);$i++)
             {
+                echo "here";
                 $appID=$am['idapplication_master'];
                 $this->__construct($appID);
                 $application=$this->getApplication();
